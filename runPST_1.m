c15e558f8e538d9d969765dbe1f39ecce3576de9
joshua <joshuaweijian.chan@connect.qut.edu.au>
2024-08-23 14:31:45 +1000

    Initial commit

---
+++ runPST_1.m
@@ -0,0 +1,24 @@
+pst1 = PST_1;
+
+% PART 1
+
+simulations = [
+    struct('P', 1, 'N', 100);    % Figure 1
+    struct('P', 1, 'N', 200);    % Figure 2
+    struct('P', rand, 'N', 100); % Figure 3
+    struct('P', rand, 'N', 200); % Figure 4
+];
+
+for i = 1:4
+    P = simulations(i).P;
+    N = simulations(i).N;
+    pst1.simulateWalk(P, N);
+end
+
+% PART 2
+
+bins = [ 20, 10, 40 ];
+
+for j = 1:3
+    pst1.sampleExperimentalData(bins(j));
+end
